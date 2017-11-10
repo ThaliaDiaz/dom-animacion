78298bd498971c83eea22a8de6852eb04da01df0
ThaliaDiaz <thalidiaz24@gmail.com>
2017-11-10 03:05:10 -0500

    agregando posicion dinamica a mi cuadro rojo al iniciar

--- js/app.js
+++ js/app.js
@@ -1,33 +1,39 @@
+var td = document.querySelectorAll('td');
 
-window .addEventListener('keyup', function(e) {
-  var ascii = e.keyCode;
-  console.log(ascii);
+window.onload = function() {
   var aleatorio = Math.round(Math.random() * 10);
+  /* var aleatorio = 6;*/
+  var position = aleatorio;
+  td[position].classList.add('red');
 
-  var td = document.querySelectorAll('td');
-  td[aleatorio].classList.add('red');
+  
+  window .addEventListener('keyup', function(e) {
+    var ascii = e.keyCode;
+    console.log(ascii);
+    td[position].classList.add('red');
+    /* flecha arriba*/ 
+    if (ascii === 38) {
+      td[position].classList.remove('red');
+      td[position - 4].classList.add('red');
+    }
+    /* flecha abajo*/ 
+    if (ascii === 40) {
+      td[position - 4].classList.remove('red');
+      td[position].classList.remove('red');
+      td[position + 4].classList.add('red');
+    }
+    /* Flecha izquierda */
+    if (ascii === 37) {
+      td[position + 4].classList.remove('red');
+      td[position].classList.remove('red');
+      td[position - 1].classList.add('red');
+    }
+    /* Flecha derecha */
+    if (ascii === 39) {
+      td[position - 1].classList.remove('red');
+      td[position].classList.remove('red');
+      td[position].classList.add('red');
+    }
+  });
+};
 
-  /* flecha arriba*/ 
-  if (ascii === 38) {
-    td[6].classList.remove('red');
-    td[2].classList.add('red');
-  }
-  /* flecha abajo*/ 
-  if (ascii === 40) {
-    td[2].classList.remove('red');
-    td[6].classList.remove('red');
-    td[10].classList.add('red');
-  }
-  /* Flecha izquierda */
-  if (ascii === 37) {
-    td[10].classList.remove('red');
-    td[6].classList.remove('red');
-    td[5].classList.add('red');
-  }
-  /* Flecha derecha */
-  if (ascii === 39) {
-    td[5].classList.remove('red');
-    td[6].classList.remove('red');
-    td[7].classList.add('red');
-  }
-});
